f38a4dce17cdf1381af167960c25769a81954510
kurogoro <kurogoro0241@gmail.com>
2019-05-12 09:00:31 +0900

    Create messages routes

--- config/routes.rb
+++ config/routes.rb
@@ -3,6 +3,6 @@ Rails.application.routes.draw do
   root 'groups#index'
   resources :users, only: [:index, :edit, :update]
   resources :groups, only: [:index, :new, :create, :edit, :update] do
-    resources :messages, only: [:create]
+    resources :messages, only: [:index, :create]
   end
 end
