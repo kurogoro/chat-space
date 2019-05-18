c2dd07b3953fdd31319330a5e480318ce24005f7
kurogoro <kurogoro0241@gmail.com>
2019-05-18 13:58:07 +0900

    Update index_user.js for ajax

--- app/assets/javascripts/index_user.js
+++ app/assets/javascripts/index_user.js
@@ -2,5 +2,16 @@ $(function() {
   $("#user-search-field").on("keyup", function() {
     var input = $("#user-search-field").val();
     // console.log(input);
+
+    $.ajax({
+      type: 'GET',
+      url: '/users',
+      data: { keyword: input },
+      dataType: 'json'
+    })
+
+    .done(function(users) {
+      console.log(users);
+    })
   });
 });
