535c6c6acd3e4422fe546508e3aa19daa281cd84
kurogoro <kurogoro0241@gmail.com>
2019-06-08 14:31:57 +0900

    Update carrierwave.rb

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -5,17 +5,17 @@ require 'carrierwave/storage/fog'
 CarrierWave.configure do |config|
   config.storage = :fog
   config.fog_provider = 'fog/aws'
-  puts "test"
-  puts "#{Rails.application.secrets.aws_access_key_id}"
-  puts "#{Rails.application.secrets.aws_secret_access_key}"
+  # puts "test"
+  # puts "#{Rails.application.secrets.aws_access_key_id}"
+  # puts "#{Rails.application.secrets.aws_secret_access_key}"
   config.fog_credentials = {
     provider: 'AWS',
     aws_access_key_id: Rails.application.secrets.aws_access_key_id,
     aws_secret_access_key: Rails.application.secrets.aws_secret_access_key,
     region: 'ap-northeast-1'
   }
-  puts "#{config.fog_credentials[:aws_access_key_id]}"
-  puts "#{config.fog_credentials[:aws_secret_access_key]}"
+  # puts "#{config.fog_credentials[:aws_access_key_id]}"
+  # puts "#{config.fog_credentials[:aws_secret_access_key]}"
 
   config.fog_directory  = 'kurogoro-bucket'
   config.asset_host = 'https://s3-ap-northeast-1.amazonaws.com/kurogoro-bucket'
